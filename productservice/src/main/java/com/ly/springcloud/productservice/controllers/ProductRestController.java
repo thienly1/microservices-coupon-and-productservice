bd0a4a3764c1efaf315602927501e48e2019c693
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-04-17 23:27:37 +0200

    DepOps for Microservices -Save data in MariaDB - store in S3 services -Deploy using EC2

---
+++ productservice/src/main/java/com/ly/springcloud/productservice/controllers/ProductRestController.java
@@ -0,0 +1,34 @@
+package com.ly.springcloud.productservice.controllers;
+
+import com.ly.springcloud.dto.Coupon;
+import com.ly.springcloud.productservice.model.Product;
+import com.ly.springcloud.productservice.repos.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/productapi")
+public class ProductRestController {
+
+    @Autowired
+    private ProductRepo repo;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Value("${couponService.url}")
+    private String couponServiceURL;
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public Product create(@RequestBody Product product){
+        Coupon coupon = restTemplate.getForObject(couponServiceURL+product.getCouponCode(), Coupon.class);
+        product.setPrice(product.getPrice().subtract(coupon != null ? coupon.getDiscount() : null));
+        return repo.save(product);
+    }
+    public Product sendErrorResponse(Product product){
+        return product;
+    }
+}
